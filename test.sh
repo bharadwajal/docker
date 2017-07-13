5176df2dedb7dece54f6b91b0eadff7ab21b634c
Bharadwaja Lanke <blanke@Resolutes-MBP-2.dh.resolutebi.com>
2017-07-13 16:54:46 -0400

    modified test

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-docker exec  -t apache netstat -apn | grep 80 | grep LISTEN | wc -l | tr -d '\n' > /Users/blanke/result
+docker exec  -t apache netstat -apn | grep 80 | grep LISTEN | wc -l | tr -d '\n' > /var/lib/jenkins/users/bj/wait_results
 
-r=`cat /Users/blanke/result`
+r=`cat /var/lib/jenkins/users/bj/wait_results`
 if [ $r == "1" ]
 then  
  {
