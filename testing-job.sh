b28273d49f1faa203c435f7b28db06fbca0eb0de
Bharadwaja Lanke <blanke@Resolutes-MBP-2.dh.resolutebi.com>
2017-07-19 13:25:50 -0400

    new shell

--- testing-job.sh
+++ testing-job.sh
@@ -4,24 +4,24 @@ image_tag=$(echo 'docker ps --format "{{.Image}}" | cut -d ':' -f 2' | bash)
 
 echo "Testing begins"
 
-if [ $image_tag == "testing" ]
+if [ $image_tag == "A" ]
 then
   echo "Tag matched. Running test job 1."
-  curl -X POST http://54.172.195.237:8080/job/docker-job/build \
+  curl -X POST http://54.172.195.237:8080/job/docker-test1/build \
   --user bj:b6475f8b607c0a88dfb82dca1fe02fe2 \
   --data-urlencode json='{"parameter": [{"name":"Name", "value":"Woohoo!"}]}'
 
-elif [ $image_tag == "integration2" ]
+elif [ $image_tag == "B" ]
 then
     echo "Tag:integration2 matched. Running jenkins test job2."
-    curl -X POST http://54.172.195.237:8080/job/docker-test-2/build \
+    curl -X POST http://54.172.195.237:8080/job/docker-test2/build \
   --user bj:b6475f8b607c0a88dfb82dca1fe02fe2 \
   --data-urlencode json='{"parameter": [{"name":"Name", "value":"Woohoo!"}]}'
   
-elif [ $image_tag == "integration3" ]
+elif [ $image_tag == "C" ]
 then
     echo "Tag:integration2 matched. Running jenkins test job3."
-    curl -X POST http://54.172.195.237:8080/job/docker-test-3/build \
+    curl -X POST http://54.172.195.237:8080/job/docker-test3/build \
   --user bj:b6475f8b607c0a88dfb82dca1fe02fe2 \
   --data-urlencode json='{"parameter": [{"name":"Name", "value":"Woohoo!"}]}'
   
@@ -31,5 +31,3 @@ else
 fi
 
 echo "Testing ends"
-
-
